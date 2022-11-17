a84b92c7885aa7197d15c4f918de7376fddf3b18
Yan2 <yan2@gmail.com>
2022-11-17 17:58:22 +0500

    drawing yellow circle

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+from random import randint as rnd
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtGui import QPainter, QColor
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+        self.do_paint = False
+        self.qp = QPainter()
+        self.draw_btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            self.qp.begin(self)
+            self.draw_circle(self.qp)
+            self.qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor('yellow'))
+        circle_size = rnd(1, 300)
+        rand_x = rnd(1, 725)
+        rand_y = rnd(1, 554)
+        qp.drawEllipse(rand_x, rand_y, rand_x + circle_size, rand_x + circle_size,)
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec_())
